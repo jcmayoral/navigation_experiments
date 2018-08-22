59ec7d30d6ef43964e36d08561992c0dd0a71b32
Jose Mayoral Banos <banos@magazino.eu>
2018-08-22 10:53:44 +0200

    adding orientations to global path

--- plot_path.py
+++ plot_path.py
@@ -14,6 +14,7 @@ class Plotter:
         self.ready = False
         self.x = list()
         self.y = list()
+        self.yaw = list()
         self.odom_x = list()
         self.odom_y = list()
         self.clear = False
@@ -29,11 +30,16 @@ class Plotter:
         self.odom_y = list()
 
         data = list()
+        yaw = list()
         for p in msg.poses:
             odom_pose = self.listener.transformPose("odom", p)
+            explicit_quaternion = [odom_pose.pose.orientation.x, odom_pose.pose.orientation.y, odom_pose.pose.orientation.z, odom_pose.pose.orientation.w]
+            euler = tf.transformations.euler_from_quaternion(explicit_quaternion)
             data.append([odom_pose.pose.position.x,odom_pose.pose.position.y])
+            yaw.append(euler[2])
         self.x =  [item[0] for item in data]
         self.y =  [item[1] for item in data]
+        self.yaw =  [i for i in yaw]
         self.ready = True
 
     def odomCB(self,msg):
@@ -44,13 +50,15 @@ class Plotter:
 plt.ion()
 
 plot = Plotter()
-
+r = 0.5
 while not rospy.is_shutdown():
     if plot.ready:
         if plot.clear:
             plt.clf()
             plot.clear = False
         plt.scatter(plot.x, plot.y, c='r')
+        for x,y,w in zip(plot.x, plot.y, plot.yaw):
+            plt.arrow(x, y,r*np.cos(w), r*np.sin(w))
         plt.scatter(plot.odom_x, plot.odom_y, c='b')
         plot.fig.canvas.draw_idle()
         plt.pause(0.1)
