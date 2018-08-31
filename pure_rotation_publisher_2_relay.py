321fb978b924f47e73a43fba0125688345679781
Jose Mayoral Banos <banos@magazino.eu>
2018-08-31 18:22:35 +0200

    relative rotation instead than in map frame

--- pure_rotation_publisher_2_relay.py
+++ pure_rotation_publisher_2_relay.py
@@ -8,15 +8,16 @@ rospy.init_node("relay_publiser")
 pub = rospy.Publisher("move_base_simple/goal", PoseStamped)
 listener = tf.TransformListener()
 listener.waitForTransform("map", "base_link", rospy.Time(0), rospy.Duration(.5))
-position, quaternion = listener.lookupTransform("map", "base_link", rospy.Time(0))
-print position, quaternion
+position, o_quaternion = listener.lookupTransform("map", "base_link", rospy.Time(0))
+print position, o_quaternion
 
 goal = PoseStamped()
 goal.header.stamp = rospy.Time.now()
 goal.header.frame_id = "map"
 goal.pose.position.x = position[0]
 goal.pose.position.y = position[1]
-quaternion = tf.transformations.quaternion_from_euler(0,0, float(sys.argv[1]))
+roll, pitch, yaw = tf.transformations.euler_from_quaternion(o_quaternion)
+quaternion = tf.transformations.quaternion_from_euler(0,0, yaw + float(sys.argv[1]))
 goal.pose.orientation.x = quaternion[0]
 goal.pose.orientation.y = quaternion[1]
 goal.pose.orientation.z = quaternion[2]
