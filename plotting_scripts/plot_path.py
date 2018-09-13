07459c5a2d73515eee0344bbe83d35c6677745ce
Jose Mayoral Banos <banos@magazino.eu>
2018-09-13 14:01:54 +0200

    more robust script in plot_path

--- plotting_scripts/plot_path.py
+++ plotting_scripts/plot_path.py
@@ -3,6 +3,7 @@ import rospy
 import numpy as np
 import matplotlib.pyplot as plt
 from nav_msgs.msg import Path, Odometry
+from copy import deepcopy
 
 class Plotter:
     def __init__(self):
@@ -18,23 +19,25 @@ class Plotter:
         self.clear = False
         self.path_received = False
         self.listener = tf.TransformListener()
+        self.listener.waitForTransform("/odom", "/map", rospy.Time(0),rospy.Duration(1.0))
+        rospy.sleep(4)
         rospy.Subscriber("/navigation/move_base_flex/SBPLLatticePlanner/plan", Path,self.pathCB)
         rospy.Subscriber("/odom",Odometry,self.odomCB)
 
-
     def pathCB(self, msg):
-        self.path_received = False
         self.odom_x = list()
         self.odom_y = list()
         self.odom_yaw = list()
-        self.listener.waitForTransform("/base_footprint", "/map", rospy.Time(0),rospy.Duration(1.0))
+        self.path_received = False
+        self.clear = True
+        self.ready = False
 
         data = list()
         yaw = list()
-        
+
         try:
             for p in msg.poses:
-                odom_pose = self.listener.transformPose("/odom", p)
+                odom_pose = self.listener.transformPose("odom", p)
                 explicit_quaternion = [odom_pose.pose.orientation.x, odom_pose.pose.orientation.y, odom_pose.pose.orientation.z, odom_pose.pose.orientation.w]
                 euler = tf.transformations.euler_from_quaternion(explicit_quaternion)
                 data.append([odom_pose.pose.position.x,odom_pose.pose.position.y])
@@ -44,21 +47,22 @@ class Plotter:
                 self.yaw =  [i for i in yaw]
                 self.ready = True
                 self.path_received = True
+            print "HACK WORKED"
         except:
             print "RECALLING"
             self.pathCB(msg)
 
     def odomCB(self,msg):
-        if self.ready:
-            return
         if not self.path_received:
             return
+        if self.ready:
+            return
+        self.ready = False
         self.odom_x.append(msg.pose.pose.position.x)
         self.odom_y.append(msg.pose.pose.position.y)
         explicit_quaternion = [msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
         euler = tf.transformations.euler_from_quaternion(explicit_quaternion)
         self.odom_yaw.append(euler[2])
-        self.clear = True
         self.ready = True
 
 plt.ion()
@@ -67,19 +71,29 @@ plot = Plotter()
 r = 0.3
 while not rospy.is_shutdown():
     if plot.ready:
+        plot.ready = False
         if plot.clear:
             plt.clf()
             plot.clear = False
-        plt.scatter(plot.x, plot.y, c='r')
-        for x,y,w in zip(plot.x, plot.y, plot.yaw):
+        print "PLOTTING"
+
+        x1 = deepcopy(plot.x)
+        y1= deepcopy(plot.y)
+        yaw1= deepcopy(plot.yaw)
+        ox= deepcopy(plot.odom_x)
+        oy= deepcopy(plot.odom_y)
+        oyaw= deepcopy(plot.odom_yaw)
+        plt.scatter(x1, y1, c='r')
+
+        for x,y,w in zip(x1, y1, yaw1):
             plt.arrow(x, y,r*np.cos(w), r*np.sin(w), color='r')
-        plt.scatter(plot.odom_x, plot.odom_y, c='b')
-        for x,y,w in zip(plot.odom_x, plot.odom_y, plot.odom_yaw):
+        plt.scatter(ox, oy, c='b')
+
+        for x,y,w in zip(ox, oy, oyaw):
             plt.arrow(x, y,r*np.cos(w), r*np.sin(w), color='b')
 
-        if len(plot.y) > 0:
-            plt.ylim(min(plot.y)-r, max(plot.y)+r)
+        if len(y1) > 0:
+            plt.xlim(min(plot.x)-r, max(plot.x)+r)
             plt.xlim(min(plot.x)-r, max(plot.x)+r)
         plot.fig.canvas.draw_idle()
         plt.pause(0.1)
-        plot.ready = False
