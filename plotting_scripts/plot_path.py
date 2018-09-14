51dd7c69d1586f297c18ec2cbd8ec52b5abce54d
Jose Mayoral Banos <banos@magazino.eu>
2018-09-14 16:19:26 +0200

    mutex to plot_path

--- plotting_scripts/plot_path.py
+++ plotting_scripts/plot_path.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 from nav_msgs.msg import Path, Odometry
 from geometry_msgs.msg import PoseStamped
 from copy import deepcopy
+import threading
 
 class Plotter:
     def __init__(self):
         rospy.init_node("Path_Plotter")
         self.fig, self.ax = plt.subplots()
-        self.ready = False
         self.x = list()
         self.y = list()
         self.yaw = list()
@@ -18,7 +18,7 @@ class Plotter:
         self.odom_y = list()
         self.odom_yaw = list()
         self.clear = False
-        self.path_received = False
+        self.lock = threading.Lock()
         self.listener = tf.TransformListener()
         rospy.Subscriber("/navigation/move_base_flex/SBPLLatticePlanner/plan", Path,self.pathCB)
         rospy.Subscriber("/odom",Odometry,self.odomCB, queue_size = 1)
@@ -26,12 +26,11 @@ class Plotter:
         rospy.loginfo("PLOT READY")
 
     def pathCB(self, msg):
+        self.lock.acquire()
         self.odom_x = list()
         self.odom_y = list()
         self.odom_yaw = list()
-        self.path_received = False
         self.clear = True
-        self.ready = False
 
         data = list()
         yaw = list()
@@ -46,16 +45,11 @@ class Plotter:
         self.y =  [item[1] for item in data]
         self.yaw =  [i for i in yaw]
 
-        self.ready = True
-        self.path_received = True
+        self.lock.release()
 
     def odomCB(self,msg):
-        if not self.path_received:
-            return
-        if self.ready:
-            return
+        self.lock.acquire()
 
-        self.ready = False
         p = PoseStamped()
         p.header = msg.header
         p.header.stamp = rospy.Time.now()
@@ -69,38 +63,38 @@ class Plotter:
         self.odom_x.append(odom_pose.pose.position.x)
         self.odom_y.append(odom_pose.pose.position.y)
         self.odom_yaw.append(euler[2])
-        self.ready = True
-        print "HACK WORKED"
+        self.lock.release()
 
 plt.ion()
 
 plot = Plotter()
 r = 0.3
 while not rospy.is_shutdown():
-    if plot.ready:
-        plot.ready = False
-        if plot.clear:
-            plt.clf()
-            plot.clear = False
-        print "PLOTTING"
+    plot.lock.acquire()
 
-        x1 = deepcopy(plot.x)
-        y1= deepcopy(plot.y)
-        yaw1= deepcopy(plot.yaw)
-        ox= deepcopy(plot.odom_x)
-        oy= deepcopy(plot.odom_y)
-        oyaw= deepcopy(plot.odom_yaw)
-        plt.scatter(x1, y1, c='r')
+    if plot.clear:
+        plt.clf()
+        plot.clear = False
 
-        for x,y,w in zip(x1, y1, yaw1):
-            plt.arrow(x, y,r*np.cos(w), r*np.sin(w), color='r')
-        plt.scatter(ox, oy, c='b')
+    x1 = deepcopy(plot.x)
+    y1= deepcopy(plot.y)
+    yaw1= deepcopy(plot.yaw)
+    ox= deepcopy(plot.odom_x)
+    oy= deepcopy(plot.odom_y)
+    oyaw= deepcopy(plot.odom_yaw)
+    plt.scatter(x1, y1, c='r')
 
-        for x,y,w in zip(ox, oy, oyaw):
-            plt.arrow(x, y,r*np.cos(w), r*np.sin(w), color='b')
+    for x,y,w in zip(x1, y1, yaw1):
+       plt.arrow(x, y,r*np.cos(w), r*np.sin(w), color='r')
+    plt.scatter(ox, oy, c='b')
 
-        if len(y1) > 0:
-            plt.xlim(min(plot.x)-r, max(plot.x)+r)
-            plt.xlim(min(plot.x)-r, max(plot.x)+r)
-        plot.fig.canvas.draw_idle()
-        plt.pause(0.1)
+    for x,y,w in zip(ox, oy, oyaw):
+       plt.arrow(x, y,r*np.cos(w), r*np.sin(w), color='b')
+
+    if len(y1) > 0:
+       plt.xlim(min(plot.x)-r, max(plot.x)+r)
+       plt.xlim(min(plot.x)-r, max(plot.x)+r)
+    
+    plot.fig.canvas.draw_idle()
+    plot.lock.release()
+    plt.pause(0.1)
