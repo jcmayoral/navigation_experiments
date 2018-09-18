347a93b2a35f8b1f0a28b84f28f61f54d5c929f3
Jose Mayoral Banos <banos@magazino.eu>
2018-09-18 13:40:59 +0200

    refactor execution tools...cmd_vel instead of imu

--- action_scripts/utilities/execution_tools.py
+++ action_scripts/utilities/execution_tools.py
@@ -1,9 +1,7 @@
-import numpy as np
 import tf
 import rospy
 from nav_msgs.msg import Path, Odometry
-from geometry_msgs.msg import PoseStamped
-from sensor_msgs.msg import Imu
+from geometry_msgs.msg import PoseStamped, Twist, Vector3
 from message_filters import TimeSynchronizer, Subscriber, ApproximateTimeSynchronizer
 from mag_common_py_libs.geometry import yaw
 
@@ -26,34 +24,42 @@ class PathConstructor:
 
 class ExecutionAnalyzer:
     def __init__(self):
-        self.tss = ApproximateTimeSynchronizer([Subscriber("/odom",Odometry), Subscriber("/imu/data_raw_transformed", Imu)],5,0.1)
+        self.tss = ApproximateTimeSynchronizer([Subscriber("/odom",Odometry), Subscriber("/cmd_vel", Twist)],5,0.1, allow_headerless=True)
         self.tss.registerCallback(self.got_velocities)
-        self.accumulate_error = [0,0,0]
+        self.accumulated_error = [0,0,0]
         self.samples_number = 0
-        self.accumulated_vel = 0.0
+        self.accumulated_vel = Twist()
+        #Defining Operators between Twist messgaes
+        self.accumulateSpeed = lambda t1, t2: Twist(linear=Vector3(x=t1.linear.x + t2.linear.x), angular=Vector3(z=t1.angular.z + t2.angular.z))
+        self.diffSpeed = lambda t1, t2: Twist(linear=Vector3(x=t1.linear.x - t2.linear.x), angular=Vector3(z=t1.angular.z - t2.angular.z))
 
     def reset(self):
         print "RESET ANALYZER"
         self.samples_number = 0
-        self.accumulate_error = [0.,0.,0.]
-        self.accumulated_vel = 0.0
+        self.accumulated_error = [0.,0.]
+        self.accumulated_vel = Twist()
 
     def get_accumulated_error(self):
-        return self.accumulate_error
+        self.accumulated_error[0] = self.accumulated_vel.linear.x
+        self.accumulated_error[1] = self.accumulated_vel.angular.z
+        return self.accumulated_error
+
+    def get_accumulated_velocities(self):
+        return [self.accumulated_vel.linear.x, self.accumulated_vel.angular.z]
 
     def get_mean_accumulated_error(self):
-        if self.samples_number > 0:
-            return [x / self.samples_number for x in self.accumulate_error]
-        rospy.logwarn("SAMPLES NUMBER IS 0")
-        return self.accumulate_error
+        self.accumulated_error[0] = self.accumulated_error.linear.x
+        self.accumulated_error[1] = self.accumulated_error.angular.z
 
-    def got_velocities(self, odom, imu):
-        if imu.linear_acceleration.x > 0.08 or np.fabs(imu.angular_velocity.z) > 0.008:
-            self.accumulated_vel+= imu.linear_acceleration.x
-            diff_x = self.accumulated_vel - odom.twist.twist.linear.x
-            diff_z = imu.angular_velocity.z - odom.twist.twist.angular.z
-            diff_yaw = yaw(imu.orientation) - yaw(odom.pose.pose.orientation)
-            self.accumulate_error[0] += diff_x
-            self.accumulate_error[1] += diff_z
-            self.accumulate_error[2] += diff_yaw
-            self.samples_number+=1
+        if self.samples_number > 0:
+            return [x / self.samples_number for x in self.accumulated_error]
+        rospy.logwarn("SAMPLES NUMBER IS 0")
+        return self.accumulated_error
+
+    def got_velocities(self, odom, current_vel):
+        self.accumulated_vel= self.accumulateSpeed(self.accumulated_vel, current_vel)
+        diff_x = current_vel.linear.x - odom.twist.twist.linear.x
+        diff_z = current_vel.angular.z - odom.twist.twist.angular.z
+        self.accumulated_error[0] += diff_x
+        self.accumulated_error[1] += diff_z
+        self.samples_number+=1
