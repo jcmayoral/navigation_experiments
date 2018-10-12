3d0b597fd71bc6ade1a53fe1e6a3ad522ebf038e
Jose Mayoral Banos <banos@magazino.eu>
2018-10-12 11:14:49 +0200

    updating param values for wheels_sync

--- magazino_fdd/magazino_fdd/src/fdd_monitor_py/wheels_sync.py
+++ magazino_fdd/magazino_fdd/src/fdd_monitor_py/wheels_sync.py
@@ -8,8 +8,9 @@ from copy import deepcopy
 class WheelSyncronizer:
     def __init__(self):
         rospy.init_node("wheels_sync")
+        #values copied from the diffdrive_controller file not available on parameter server
         self.R = 0.5
-        self.L = 1
+        self.L = rospy.get_param("/filtered/canopen_interface/differential_drive/wheel_separation")
         self.odom_vel_pub = rospy.Publisher("recomputed_odom_vel", Twist, queue_size = 1)
         self.diff_pub = rospy.Publisher("magazino_fdd/wheel_diff", Float64, queue_size = 1)
         self.tss = ApproximateTimeSynchronizer([Subscriber("/left_speed",Float64), Subscriber("/right_speed", Float64)],5,0.1, allow_headerless=True)
