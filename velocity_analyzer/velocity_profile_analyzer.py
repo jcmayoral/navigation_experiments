f091733c98193ef0adf60b78b78e6733f702485e
Jose Mayoral Banos <banos@magazino.eu>
2018-10-15 17:28:15 +0200

    removing get_max function

--- velocity_analyzer/velocity_profile_analyzer.py
+++ velocity_analyzer/velocity_profile_analyzer.py
@@ -22,33 +22,26 @@ getOdomAccel = lambda odom_vel, last_odom_vel: Twist(linear=Vector3(x=odom_vel.t
 def velocities_cb(odom_msg, cmd_msg):
     global max_error
     diff_twist = diffSpeed(cmd_msg, odom_msg)
-    max_error.linear.x = get_max(diff_twist.linear.x, max_error.linear.x)
-    max_error.angular.z = get_max(diff_twist.angular.z, max_error.angular.z)
-
-
-def get_max (val_a, val_b):
-    if val_a > val_b:
-        return val_a
-    else:
-        return val_b
+    max_error.linear.x = max(diff_twist.linear.x, max_error.linear.x)
+    max_error.angular.z = max(diff_twist.angular.z, max_error.angular.z)
 
 def cmd_cb(cmd_msg):
     global last_odom_vel, max_error
     diff_twist = diffSpeed(cmd_msg, last_odom_vel)
-    max_error.linear.x = get_max(diff_twist.linear.x, max_error.linear.x)
-    max_error.angular.z = get_max(diff_twist.angular.z, max_error.angular.z)
+    max_error.linear.x = max(diff_twist.linear.x, max_error.linear.x)
+    max_error.angular.z = max(diff_twist.angular.z, max_error.angular.z)
 
     global max_commanded_accel, last_cmd_vel
     diff_twist = getAccel(cmd_msg, last_cmd_vel)
-    max_commanded_accel.linear.x = get_max(diff_twist.linear.x, max_commanded_accel.linear.x)
-    max_commanded_accel.angular.z = get_max(diff_twist.angular.z, max_commanded_accel.angular.z)
+    max_commanded_accel.linear.x = max(diff_twist.linear.x, max_commanded_accel.linear.x)
+    max_commanded_accel.angular.z = max(diff_twist.angular.z, max_commanded_accel.angular.z)
     last_cmd_vel = cmd_msg
 
 def odom_cb(odom_msg):
     global last_odom_vel, max_reported_accel
     diff_twist = getOdomAccel(odom_msg, last_odom_vel)
-    max_reported_accel.linear.x = get_max(diff_twist.linear.x, max_reported_accel.linear.x)
-    max_reported_accel.angular.z = get_max(diff_twist.angular.z, max_reported_accel.angular.z)
+    max_reported_accel.linear.x = max(diff_twist.linear.x, max_reported_accel.linear.x)
+    max_reported_accel.angular.z = max(diff_twist.angular.z, max_reported_accel.angular.z)
     last_odom_vel = odom_msg
 
 
