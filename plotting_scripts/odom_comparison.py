277e840537fd080b869286cc0aeb9b733424639c
Jose Mayoral Banos <banos@magazino.eu>
2018-10-17 11:38:12 +0200

    comparing odometries

--- plotting_scripts/odom_comparison.py
+++ plotting_scripts/odom_comparison.py
@@ -13,6 +13,7 @@ class OdomPlotter:
         self.x = 0
         self.y = 0
         self.yaw = 0
+        self.is_started = False
         self.clear = False
         self.lock = threading.Lock()
         self.listener = tf.TransformListener()
@@ -20,6 +21,7 @@ class OdomPlotter:
         rospy.loginfo("PLOT READY")
 
     def odomCB(self,msg):
+        self.is_started = True
         odom_pose = PoseStamped()
         odom_pose.header = msg.header
         odom_pose.pose = msg.pose.pose
@@ -35,10 +37,9 @@ class OdomPlotter:
 rospy.init_node("odom_comparison_plot")
 plt.ion()
 fig, ax = plt.subplots()
-
 plot = OdomPlotter(topic_name="/odom")
-ekf_plot = OdomPlotter(topic_name="/odometry/filtered")
-r = 0.3
+ekf_plot = OdomPlotter(topic_name="/fake_odom")
+r = 3.0
 
 while not rospy.is_shutdown():
 
@@ -46,29 +47,29 @@ while not rospy.is_shutdown():
         plt.clf()
         plot.clear = False
 
-    plot.lock.acquire()
-    x1 = deepcopy(plot.x)
-    y1= deepcopy(plot.y)
-    yaw1= deepcopy(plot.yaw)
-    plot.lock.release()
+    if plot.is_started and ekf_plot.is_started:
+        ax.legend()
 
-    ekf_plot.lock.acquire()
-    ox= deepcopy(ekf_plot.x)
-    oy= deepcopy(ekf_plot.y)
-    oyaw= deepcopy(ekf_plot.yaw)
-    ekf_plot.lock.release()
+        plot.lock.acquire()
+        x1 = deepcopy(plot.x)
+        y1= deepcopy(plot.y)
+        yaw1= deepcopy(plot.yaw)
+        plot.lock.release()
 
-    plt.scatter(x1, y1, c='r')
-    #for x,y,w in zip(x1, y1, yaw1):
-    plt.arrow(x1, y1,r*np.cos(yaw1), r*np.sin(yaw1), color='r')
-
-    plt.scatter(ox, oy, c='b')
-    plt.arrow(ox, ox,r*np.cos(oyaw), r*np.sin(oyaw), color='b')
-
-    """
-    if len(y1) > 0:
-       plt.xlim(min(plot.x)-r, max(plot.x)+r)
-       plt.xlim(min(plot.x)-r, max(plot.x)+r)
-    """
-    fig.canvas.draw_idle()
-    plt.pause(0.1)
+        ekf_plot.lock.acquire()
+        ox= deepcopy(ekf_plot.x)
+        oy= deepcopy(ekf_plot.y)
+        oyaw= deepcopy(ekf_plot.yaw)
+        ekf_plot.lock.release()
+        plt.scatter(x1, y1, c='r')
+        #for x,y,w in zip(x1, y1, yaw1):
+        plt.arrow(x1, y1,r*np.cos(yaw1), r*np.sin(yaw1), color='r',  label='odom')
+        plt.scatter(ox, oy, c='b')
+        plt.arrow(ox, oy,r*np.cos(oyaw), r*np.sin(oyaw), color='b',  label='fake_odom')
+        """
+        if len(y1) > 0:
+            plt.xlim(min(plot.x)-r, max(plot.x)+r)
+            plt.xlim(min(plot.x)-r, max(plot.x)+r)
+        """
+        fig.canvas.draw_idle()
+        plt.pause(0.1)
