ee037a995845c8f353ebb9bb8a2b820ca0d22f06
Jose Mayoral Banos <banos@magazino.eu>
2018-10-23 11:07:13 +0200

    tf compensation frames to variable (two params required)

--- move_base_utilities/localization_launch_files/tf_compensation_counter.py
+++ move_base_utilities/localization_launch_files/tf_compensation_counter.py
@@ -8,22 +8,25 @@ from std_msgs.msg import Float32
 
 rospy.init_node("cartographer_tf_interventions")
 
+map_frame = "map"
+odom_frame = "odom"
+
 fb_publish = rospy.Publisher("tf_error", Float32, queue_size = 5)
 listener = tf.TransformListener()
-listener.waitForTransform("map_carto", "odom", rospy.Time(0), rospy.Duration(10.0))
-old_pose, old_orientation = listener.lookupTransform("map_carto", "odom", rospy.Time(0))
+listener.waitForTransform(map_frame, odom_frame, rospy.Time(0), rospy.Duration(10.0))
+old_pose, old_orientation = listener.lookupTransform(map_frame, odom_frame, rospy.Time(0))
 old_time = rospy.Time.now()
 tf_corrections = 0
 accumulated_error = 0.0
 angular_accumulated_error = 0.0
 detected_delays = 0
-tolerance = 0.015
-ang_tolerance = 0.17
+tolerance = -1.0
+ang_tolerance = -1.0
 max_delay = 0.05
 
 while not rospy.is_shutdown():
-    listener.waitForTransform("map_carto", "odom", rospy.Time(0), rospy.Duration(1.0))
-    p, o = listener.lookupTransform("map_carto", "odom", rospy.Time(0))
+    listener.waitForTransform(map_frame, odom_frame, rospy.Time(0), rospy.Duration(1.0))
+    p, o = listener.lookupTransform(map_frame, odom_frame, rospy.Time(0))
 
     pose_diff = [True if fabs(x-y)> tolerance else False for (x,y) in zip(p,old_pose)]
     #pose_diff = [fabs(x - y) for (x, y) in zip(p, old_pose)]
@@ -50,5 +53,5 @@ while not rospy.is_shutdown():
 
 rospy.loginfo("Accumulated error %f", accumulated_error)
 rospy.loginfo("Accumulated angular error %f", angular_accumulated_error)
-rospy.loginfo("interventions found %i", tf_corrections)
+rospy.loginfo("tf corrections found %i", tf_corrections)
 rospy.loginfo("delays found %i", detected_delays)
